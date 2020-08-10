d19981972144e35d59761acd82ee2c4fd3029d4d
han.wang <han.wang@aqrose.com>
2020-08-10 18:36:24 +0800

    add 121

--- 121.买卖股票的最佳时机.cpp
+++ 121.买卖股票的最佳时机.cpp
@@ -2,19 +2,29 @@
  * @lc app=leetcode.cn id=121 lang=cpp
  *
  * [121] 买卖股票的最佳时机
+ * 
+ * 思路：记录最小值，向后面找比它大的，计算差值，迭代插值。
+ * 若遇到更小的，更新最小值，继续向后走，计算差值，更新差值。
  */
 
 // @lc code=start
 class Solution {
 public:
     int maxProfit(vector<int>& prices) {
-        int max = 0;
-        int min = 9999;
-        int max_index = -1;
-        int min_index = -1;
-        for(int i = 0;i<prices.size();i++){
-            
+        if(prices.size() == 0)
+            return 0;
+        int res = 0;
+        int pre = prices[0];
+        for(int i = 1;i<prices.size();i++){
+            if(prices[i] < pre){
+                pre = prices[i];
+            }
+            int current = prices[i] - pre;
+            if(current > res){
+                res = current;
+            }
         }
+        return res;
     }
 };
 // @lc code=end
