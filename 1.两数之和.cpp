aadb0332bc0f04fa020116ea0d8730b0f476e451
han.wang <han.wang@aqrose.com>
2020-08-17 18:14:37 +0800

    modify 01

--- 1.两数之和.cpp
+++ 1.两数之和.cpp
@@ -8,18 +8,22 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        vector<int> result;
-        for(int i=0;i<nums.size() -1 ;i++){
-            int another = target - nums.at(i);
-            for(int j=i+1;j<nums.size();j++){
-                if(nums.at(j) == another){
-                    result.push_back(i);
-                    result.push_back(j);
-                    break;
-                }
+        vector<int> res;
+        int size = nums.size();
+        vector<pair<int,int>> arr(size);
+        for(int i = 0;i< size;i++){
+            arr[i] = make_pair(nums[i], i);
+        }
+        sort(arr.begin(), arr.end(), [](const pair<int, int> &a,const pair<int, int> &b){return a.first< b.first;});
+
+        for(int i =0;i<size -1;i++){
+            int pos = lower_bound(arr.begin() + i+1, arr.end(), target - arr[i].first, [](const pair<int,int> &a, const int &b){ return a.first < b; }) - arr.begin();
+            if(pos != size && arr[pos].first + arr[i].first == target){
+                res.push_back(arr[i].second);
+                res.push_back(arr[pos].second);
             }
         }
-        return result;
+        return res;
     }
 };
 // @lc code=end
