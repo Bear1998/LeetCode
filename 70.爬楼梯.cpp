b46aefa58d7cecf9c18f5c687b376c51ab6bc34f
han.wang <wanghan408812@gmail.com>
2020-08-24 17:12:44 +0800

    modify 70

--- 70.爬楼梯.cpp
+++ 70.爬楼梯.cpp
@@ -8,16 +8,18 @@
 class Solution {
 public:
     int climbStairs(int n) {
-        vector<int > vector_num;
-        vector_num.push_back(1);
-        vector_num.push_back(2);
-        vector_num.push_back(3);
-
-        for(int i = 3;i < n; i++){
-            vector_num.push_back(vector_num[i-2] + vector_num[i-1]);
+        if(n < 4)
+            return n;
+        else{
+            int num1 = 2;
+            int num2 = 3;
+            for(int i = 3;i < n; i++){
+                int num = num1 + num2;
+                num1 = num2;
+                num2 = num;
+            }
+            return num2;
         }
-        return vector_num[n-1];
-
     }
 };
 // @lc code=end
